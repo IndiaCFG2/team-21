b440c7a5fea739445aa8066ae2961c7317e2fbc0
Simarpreet Kaur <kaur.simarpreet524@gmail.com>
2020-08-08 16:28:25 +0530

    db model files addition

--- Backend/controllers/browseController.js
+++ Backend/controllers/browseController.js
@@ -1,18 +1,21 @@
 const ylink = require('../db/models/youtubeLinkModel')
+const alink = require('../db/models/articleLinkModel')
+const qlink = require('../db/models/queriesModel')
+const aqlink = require('../db/models/askQueryModel')
 
 
 module.exports.getYoutubeLinks = async(req,res)=>{
 
 }
 
-module.exports.getYoutubeLinks = async(req,res)=>{
+module.exports.getArticlesLinks = async(req,res)=>{
 
 }
 
-module.exports.getYoutubeLinks = async(req,res)=>{
+module.exports.getQueries = async(req,res)=>{
 
 }
 
-module.exports.getYoutubeLinks = async(req,res)=>{
+module.exports.postQuery = async(req,res)=>{
 
 }
--- Backend/routes/browseRoutes.js
+++ Backend/routes/browseRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const { } = require('../controllers/browseController')
+const {getYoutubeLinks, getArticlesLinks, getQueries, postQuery } = require('../controllers/browseController')
 
 router.get('/youtube',getYoutubeLinks);
-router.get('/article',getArticles);
+router.get('/article',getArticlesLinks);
 router.get('/queries',getQueries);
 
 router.post('/askquery',postQuery);
+
+module.exports = router;
